729f1c18daa6907f07f822961de803c37937158a
Tim <ticharli@student.s19.be>
2026-01-15 16:30:30 +0100

    inout

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: ticharli <ticharli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 17:39:25 by csimonne          #+#    #+#             */
-/*   Updated: 2026/01/15 16:25:49 by ticharli         ###   ########.fr       */
+/*   Updated: 2026/01/15 16:28:40 by ticharli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,12 +123,12 @@ static void	child_process(t_main *m, t_cmd_table *cmd, int prev_fd,
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
 	}
-	// if (handle_redirections(cmd))
-	// 	exit(1);
-	if (cmd->infile && !handle_redir_in(cmd->infile)) // <--- AJOUT ICI
-		return ;
-	if (cmd->outfile && !handle_redir_out(cmd->outfile)) // <--- AJOUT ICI
-		return ;
+	if (handle_redirections(cmd))
+	 	exit(1);
+	// if (cmd->infile && !handle_redir_in(cmd->infile)) // <--- AJOUT ICI
+	// 	return ;
+	// if (cmd->outfile && !handle_redir_out(cmd->outfile)) // <--- AJOUT ICI
+	// 	return ;
 	if (is_builtin(cmd->args[0]))
 		exit(run_builtin(m, cmd));
 	else
