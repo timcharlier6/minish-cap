b8994bd0f16dabb225f1e344fc428a0599bbd37f
csimonne <camillesimonney@gmail.com>
2026-01-15 15:26:05 +0100

    ajout de redirections.c

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: csimonne <csimonne@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 17:39:25 by csimonne          #+#    #+#             */
-/*   Updated: 2026/01/14 19:56:29 by csimonne         ###   ########.fr       */
+/*   Updated: 2026/01/15 15:09:23 by csimonne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,10 @@ static void	child_process(t_main *m, t_cmd_table *cmd, int prev_fd,
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
 	}
+	if (cmd->infile && !handle_redir_in(cmd->infile)) // <--- AJOUT ICI
+		return ;
+	if (cmd->outfile && !handle_redir_out(cmd->outfile)) // <--- AJOUT ICI
+		return ;
 	if (is_builtin(cmd->args[0]))
 		exit(run_builtin(m, cmd));
 	else
